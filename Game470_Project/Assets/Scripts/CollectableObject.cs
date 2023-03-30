9bdfcf4b68dc2e84815000b7476a05d6cd86ea45
Clay <cvaughn607@gmail.com>
2023-03-30 10:15:13 -0400

    Polishing

--- Game470_Project/Assets/Scripts/CollectableObject.cs
+++ Game470_Project/Assets/Scripts/CollectableObject.cs
@@ -8,13 +8,13 @@ public class CollectableObject : Interact
     public override void Interaction(GameObject player)
     {
         PlayerController playerScript = player.GetComponent<PlayerController>();
+        gameObject.GetComponentInChildren<Collider>().enabled = false;
         transform.parent = playerScript.holdPoint.transform;
         transform.position = playerScript.holdPoint.transform.position;
         transform.localEulerAngles = new Vector3(0,0,0);
         playerScript.heldObject = gameObject;
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         playerScript.holdObjectRig.weight = 1;
-        gameObject.GetComponentInChildren<Collider>().enabled = false;
         GameManager.canPlayer.interact = false;
     }
 }
