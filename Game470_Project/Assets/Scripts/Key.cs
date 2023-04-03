d2f962fe6805a529fbadf0e77bfa097fcabf216f
Clay <cvaughn607@gmail.com>
2023-04-02 21:38:22 -0400

    Level Redesign (key)

---
+++ Game470_Project/Assets/Scripts/Key.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : Interact
+{
+    public override void Interaction(GameObject player)
+    {
+        PlayerController playerScript = player.GetComponent<PlayerController>();
+        gameObject.GetComponentInChildren<Collider>().enabled = false;
+        transform.parent = playerScript.holdPoint.transform;
+        transform.position = playerScript.holdPoint.transform.position;
+        transform.localEulerAngles = new Vector3(0, 0, 0);
+        playerScript.heldObject = gameObject;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        playerScript.holdObjectRig.weight = 1;
+        GameManager.canPlayer.interact = false;
+    }
+
+    public override string InteractionText(GameObject heldObject)
+    {
+        return "Grab " + Name + " Key";
+    }
+}
