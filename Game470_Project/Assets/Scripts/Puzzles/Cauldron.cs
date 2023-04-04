d6951baeda2a0bfc0c08cd392abcae26db7fc9b2
Hypergamer33 <112579693+Hypergamer33@users.noreply.github.com>
2023-04-04 15:22:22 -0400

    Implemented Cauldron
    When all ingredients have been added, the player can brew the potion (The potion is currently nonfunctional)

--- Game470_Project/Assets/Scripts/Puzzles/Cauldron.cs
+++ Game470_Project/Assets/Scripts/Puzzles/Cauldron.cs
@@ -6,45 +6,92 @@ public class Cauldron : Interact
 {
 
     public bool ingredientsPrepared = false;
+    public bool potionBrewed = false;
+
+    public GameObject blood, eyes, ashes, petals;
+    public GameObject potion;
     public List<GameObject> ingredientsList;
 
     public override void Interaction(GameObject player)
     {
         PlayerController playerScript = player.GetComponent<PlayerController>();
-        if(playerScript.heldObject && playerScript.heldObject.TryGetComponent<CollectableObject>(out CollectableObject objectScript))
+        if(!ingredientsPrepared)
         {
-            switch (objectScript.Name)
+            if (playerScript.heldObject && playerScript.heldObject.TryGetComponent<CollectableObject>(out CollectableObject objectScript))
             {
-                case "Eyes": 
-                case "Petal": 
-                case "Ashes":
-                    {
-                        if (!ingredientsList.Contains(playerScript.heldObject))
+                switch (objectScript.Name)
+                {
+                    case "Eyes":
+                    case "Petals":
+                    case "Ashes":
                         {
-                            playerScript.heldObject.transform.SetParent(gameObject.transform);
-                            playerScript.heldObject.transform.position = gameObject.transform.position;
-                            ingredientsList.Add(playerScript.heldObject);
-                            playerScript.heldObject = null;
+                            if (!ingredientsList.Contains(playerScript.heldObject))
+                            {
+                                playerScript.heldObject.transform.SetParent(gameObject.transform);
+                                playerScript.heldObject.transform.position = gameObject.transform.position + new Vector3(0, 0.1f, 0);
+                                ingredientsList.Add(playerScript.heldObject);
+                                playerScript.heldObject = null;
+                            }
+                            break;
                         }
-                        break;
-                    }
-                case "Knife":
-                    {
-
-                        break;
-                    }
+                    case "Knife":
+                        {
+                            if (!ingredientsList.Contains(blood))
+                            {
+                                blood.SetActive(true);
+                                ingredientsList.Add(blood);
+                            }
+                            break;
+                        }
+                }
+                if (ingredientsList.Contains(eyes) && ingredientsList.Contains(ashes) && ingredientsList.Contains(petals) && ingredientsList.Contains(blood))
+                {
+                    ingredientsPrepared = true;
+                }
             }
+        }
+        else
+        {
+            foreach (GameObject ingredient in ingredientsList)
+            {
+                if (ingredient == blood)
+                {
+                    ingredient.SetActive(false);
+                }
+                else
+                {
+                    Destroy(ingredient);
+                }
+            }
+            potionBrewed = true;
+            potion.SetActive(true);
+            gameObject.GetComponent<Collider>().enabled = false;
         }
     }
 
     public override string InteractionText(GameObject heldObject)
     {
-        if(heldObject.TryGetComponent<CollectableObject>(out CollectableObject objectScript))
-        {
-            return objectScript.Name;
-        }
         string currIngredients = "";
-        if(ingredientsList.Count > 0)
+        if (heldObject && heldObject.TryGetComponent<CollectableObject>(out CollectableObject objectScript))
+        {
+            if (objectScript.Name == "Ashes" || objectScript.Name == "Eyes" || objectScript.Name == "Petals")
+            {
+                return "Add " + objectScript.Name;
+            }
+            else if(objectScript.Name == "Knife" && !ingredientsList.Contains(blood))
+            {
+                return "Add Blood";
+            }
+            else
+            {
+                return "Not an Ingredient";
+            }
+        }
+        else if (ingredientsPrepared)
+        {
+            return "Brew Potion";
+        }
+        else if(ingredientsList.Count > 0)
         {
             foreach (GameObject ingredient in ingredientsList)
             {
